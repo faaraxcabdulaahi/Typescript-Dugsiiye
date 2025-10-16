7ab62bcceec4c8b44a2bdc01b94c3359c67ebdf5
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-10-16 19:37:26 +0300

    completed the exercise 1 of the typescript

--- 01-Exercise/index.js
+++ 01-Exercise/index.js
@@ -1,12 +1,21 @@
 var productName;
 productName = "laptop";
-// productName = 23;
+// productName = 23; 
 console.log(productName);
 var productPrice;
 productPrice = 23;
-// productPrice = "23";
+// productPrice = "23"; 
 console.log(productPrice);
 var discountAvailable;
 discountAvailable = true;
-// discountAvailable = "true";
+// discountAvailable = "true"; 
 console.log(discountAvailable);
+function getDiscount(price, discount) {
+    return price - price * discount;
+}
+console.log(getDiscount(11, 0.22));
+function printLength(x) {
+    console.log(x.length);
+}
+printLength("Hello Programmers");
+printLength(123456789);
--- 01-Exercise/index.ts
+++ 01-Exercise/index.ts
@@ -1,15 +1,30 @@
-let productName:string;
+let productName: string;
 productName = "laptop";
-// productName = 23;
+// productName = 23; 
 console.log(productName);
 
-let productPrice:number;
+let productPrice: number;
 productPrice = 23;
-// productPrice = "23";
-console.log(productPrice)
+// productPrice = "23"; 
+console.log(productPrice);
 
-let discountAvailable:boolean;
+let discountAvailable: boolean;
 discountAvailable = true;
-// discountAvailable = "true";
-console.log(discountAvailable)
+// discountAvailable = "true"; 
+console.log(discountAvailable);
+
+function getDiscount(price: number, discount: number): number {
+  return price - price * discount;
+}
+
+console.log(getDiscount(11, 0.22));
+
+
+function printLength(x: any) {
+  console.log(x.length);
+}
+
+printLength("Hello Programmers"); 
+printLength(123456789);     
+
 
