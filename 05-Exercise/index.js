0b66956f046cec06682ff182bd59af73032dfee7
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-10-26 20:42:49 +0300

    added exercise 5 of the typescript language

---
+++ 05-Exercise/index.js
@@ -0,0 +1,25 @@
+function echo(data) {
+    return data;
+}
+var theName = "Faarax Cabdulaahi Cali";
+console.log(theName);
+var theAge = 23;
+console.log(theAge);
+var familyNames = ["caasho", "cismaan", "Geele"];
+var bloodGroups = {
+    A: "A",
+    B: "B",
+    AB: "AB",
+    O: "O",
+};
+console.log(familyNames[0].toUpperCase());
+var stringResult = {
+    status: "success",
+    data: "Hello World",
+};
+console.log(stringResult);
+var userResult = {
+    status: "success",
+    data: { id: 1, name: "Barasho" },
+};
+console.log(userResult);
---
+++ 05-Exercise/index.ts
@@ -0,0 +1,50 @@
+function echo<T>(data: T): T {
+  return data;
+}
+
+const theName: string = "Faarax Cabdulaahi Cali";
+console.log(theName);
+
+const theAge: number = 23;
+console.log(theAge);
+
+const familyNames: string[] = ["caasho", "cismaan", "Geele"];
+
+
+type bloodParameter = {
+    key:string;
+    
+}
+
+const bloodGroups: { [key: string]: string } = {
+  A: "A",
+  B: "B",
+  AB: "AB",
+  O: "O",
+};
+
+console.log(familyNames[0].toUpperCase());
+
+interface ApiResult<T> {
+  status: string;
+  data: T;
+}
+
+const stringResult: ApiResult<string> = {
+  status: "success",
+  data: "Hello World",
+};
+console.log(stringResult);
+
+
+
+type User = {
+  id: number;
+  name: string;
+};
+const userResult: ApiResult<User> = {
+  status: "success",
+  data: { id: 1, name: "Barasho" },
+};
+
+console.log(userResult);
